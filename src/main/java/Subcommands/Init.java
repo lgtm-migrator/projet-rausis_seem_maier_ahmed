04de5560643620cb7e4f631a1c58becd740a6443
ahmedfaroukferchichi <ahmed.ferchichi@heig-vd.ch>
2022-03-30 15:06:25 +0200

    commande init créer !

--- src/main/java/Subcommands/Init.java
+++ src/main/java/Subcommands/Init.java
@@ -1,6 +1,6 @@
 package Subcommands;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,15 +13,38 @@ import java.util.concurrent.Callable;
 
 @Command(name = "init", description = "init")
 public class Init implements Callable<Integer> {
-    @Override public Integer call() {
-        Path path =  Paths.get("/mon/site");
+    @Override public Integer call() throws IOException {
+
+        System.out.println("Init");
+        String root =  "/mon site";
+        Path path = Paths.get(root);
+
         if(Files.exists(path)){
-          
+          System.out.println("The diractory  'mon site' was already created ");
         } else {
+            // we creat the  diractory mon site
+            File f = new File(root);
+            if(f.mkdir() == true){
+                // if the diractory "mon site" was succsfuly created, we creat the file contaning site's informations
+            File site_information  = new File("site_informations");
+            if(site_information.createNewFile() == true){
+                // we wirte in the file the site's information
+                FileWriter fw  =  new FileWriter(site_information.getAbsoluteFile());
+                BufferedWriter bw = new BufferedWriter(fw);
+                // !!!!!! you have to modifie later !
+                bw.write("site information");
+                bw.close();
+            } else {
+                System.out.println("problems appeared while creating the file 'mon_site'");
+            }
+
+            } else {
+                System.out.println("problems appeared while creating the diractory 'mon site'");
+            }
 
         }
 
-        System.out.println("Init");
+
         return 0;
     }
 
