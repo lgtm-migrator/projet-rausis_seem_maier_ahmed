54ea0d50940aa77c96a124fd8b7414bd66056031
Rausis Justin <justin.rausis@heig-vd.ch>
2022-03-30 16:31:16 +0200

    Utilise les commandes

--- src/main/java/Subcommands/Build.java
+++ src/main/java/Subcommands/Build.java
@@ -1,5 +1,6 @@
 package Subcommands;
 
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 
 import java.util.concurrent.Callable;
@@ -8,9 +9,15 @@ import tools.FileManager;
 
 @Command(name = "build", description = "Build")
 public class Build implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "The path to the folder.")
+    private String path;
+
     @Override public Integer call() {
+        String longPath = System.getProperty("user.dir") + "\\" + path;
+        System.out.println(longPath);
         FileManager f = new FileManager();
-        f.build("C:\\Ecole\\DIL\\test");
+        f.build(longPath);
         return 0;
     }
 }
