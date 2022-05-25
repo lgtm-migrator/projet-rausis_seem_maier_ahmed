beb295b951bf18d370d7922a57681e8a1a85cd45
Rausis Justin <justin.rausis@heig-vd.ch>
2022-05-25 16:16:02 +0200

    Code non testé

--- src/main/java/tools/GitHelper.java
+++ src/main/java/tools/GitHelper.java
@@ -2,24 +2,28 @@ package tools;
 
 import org.eclipse.jgit.api.*;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class GitHelper {
-    private String directory;
+    private File directory;
+    private Git git;
+    private StoredConfig config;
 
     /**
      * Constructeur de la classe le répertoire à push sur le répo git distant
      * (Dans notre cas le dossier build)
      * @param directory le répertoire à push
      */
-    public GitHelper(String directory){
+    public GitHelper(File directory) throws GitAPIException {
         this.directory = directory;
+        git = Git.init().setDirectory(directory).call();
+        config = git.getRepository().getConfig();
     }
 
     /**
@@ -30,7 +34,32 @@ public class GitHelper {
      * Si la publication ne fonctionne pas efface le répo et les crédentials enregistrés
      */
     public void publish(){
+        if(!hasRemoteUrl()) askAndSaveRemoteUrl();
+        if(!hasCredentials()) askAndSaveCredentails();
 
+        //Créer le commit et le push
+        try {
+            AddCommand add = git.add();
+            add.addFilepattern(".").call();
+            CommitCommand commit = git.commit();
+            commit.setMessage("Automatique commit").call();
+
+            PushCommand push = git.push();
+            push.setCredentialsProvider(getCredentials());
+            push.call();
+        } catch (Exception e){
+            //Si une erreur survient lors du push (poblème d'accès au répo)
+            //Delete les informations sauvegardées pour en redemander des nouvelles
+            //Lors du prochain publish de l'utilisateur
+            System.out.println(e.getMessage());
+            System.out.println("Une erreur est survenue lors de la publication");
+            delSaveRemoteUrlAndCredentials();
+        }
+    }
+
+    private CredentialsProvider getCredentials(){
+        String token = config.getString("credentials", "token", "token");
+        return new UsernamePasswordCredentialsProvider(token, "");
     }
 
     /**
@@ -38,7 +67,8 @@ public class GitHelper {
      * @return true si un répo est enregistré
      */
     private boolean hasRemoteUrl(){
-        return false;
+        String url = config.getString("remote", "origin", "url");
+        return url != null;
     }
 
     /**
@@ -46,14 +76,18 @@ public class GitHelper {
      * @return tru si des crédentials sont enregistrés
      */
     private boolean hasCredentials(){
-        return false;
+        String token = config.getString("credentials", "token", "token");
+        return token != null;
     }
 
     /**
      * Demande à l'utilisateur le répo distant et le sotck dans le répertoire .git
      */
     private void askAndSaveRemoteUrl(){
-
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Entrer l'url du répertoire git distant: ");
+        String str= sc.nextLine();
+        config.setString("remote", "origin", "url", str);
     }
 
     /**
@@ -61,13 +95,16 @@ public class GitHelper {
      * (Demande uniquement le token)
      */
     private void askAndSaveCredentails(){
-
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Entrer le token pour s'authentifier: ");
+        String str= sc.nextLine();
+        config.setString("credentials", "token", "token", str);
     }
 
     /**
      * Suprpime les crédentials et l'url du répo enregistrés
      */
-    private void dalSaveRemoteUrlAndCredentials(){
+    public void delSaveRemoteUrlAndCredentials(){
         delSavedCredentials();
         delSavedRemoteUrl();
     }
@@ -76,14 +113,14 @@ public class GitHelper {
      * Supprime l'url du répo enregistré
      */
     private void delSavedRemoteUrl(){
-
+        config.setString("remote", "origin", "url", null);
     }
 
     /**
      * Supprime les crédentials enregistrés
      */
     private void delSavedCredentials(){
-
+        config.setString("credentials", "token", "token", null);
     }
 
     static public void test() throws GitAPIException, IOException {
@@ -96,7 +133,9 @@ public class GitHelper {
         config.setString("credentials", "token", "token", token);
         config.save();*/
 
-        String tokenTest = config.getString("credentials", "token", "token");
+        String test = config.getString("credentials", "token", "tokenasdf");
+
+        /*String tokenTest = config.getString("credentials", "token", "token");
         CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(tokenTest, "");
 
         AddCommand add = git.add();
@@ -106,6 +145,6 @@ public class GitHelper {
 
         PushCommand push = git.push();
         push.setCredentialsProvider(credentialsProvider);
-        push.call();
+        push.call();*/
     }
 }
