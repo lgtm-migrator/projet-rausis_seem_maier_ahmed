30f76c5f429edacd1a8a5e101b4f1e78f65a2535
Rausis Justin <justin.rausis@heig-vd.ch>
2022-06-04 20:06:57 +0200

    Ajout des commentaires de chaque méthode de FileManager.java

--- src/main/java/tools/FileManager.java
+++ src/main/java/tools/FileManager.java
@@ -7,6 +7,13 @@ import java.nio.file.*;
 public class FileManager {
     private final static String CONFIG_FILENAME = "config.json";
     private final static String LAYOUT_FILENAME = "layout.html";
+
+    /**
+     * Permet de créer un fichier avec un certain contenu
+     * @param path Le chemin absolue pour la création du fichier
+     * @param content Le contenu du fichier
+     * @return true si la création est un succès
+     */
     public static boolean createFile(String path, String content) {
         try {
             File f = new File(path);
@@ -24,6 +31,11 @@ public class FileManager {
         return true;
     }
 
+    /**
+     * Permet de créer un dossier
+     * @param path le chemin absolue du dossier à créer
+     * @return true si la création est un succès
+     */
     public static boolean createDirectory(String path) {
         try {
 
@@ -38,11 +50,22 @@ public class FileManager {
         return true;
     }
 
+    /**
+     * Permet de checker si un fichier existe
+     * @param path Le chemin absolu du fichier à checker
+     * @return true si le fichier existe
+     */
     public static boolean fileExists(String path){
         File file = new File(path);
         return file.exists();
     }
 
+    /**
+     * Permet de récupérer le contenu d'un fichier
+     * @param path le chemin absolu du fichier à récupérer le contenu
+     * @return String le contenu du fichier
+     * @throws IOException Peut lever une exception lors de l'ouverture du fichier
+     */
     public static String getContent(String path) throws IOException {
         InputStream in = new BufferedInputStream(new FileInputStream(path));
         StringBuilder content = new StringBuilder();
@@ -58,6 +81,12 @@ public class FileManager {
         return content.toString();
     }
 
+    /**
+     * Permet de copier un fichier
+     * @param from Le chemin absolu du fichier à copier
+     * @param to Le chemin absolu de l'endroit ou le copier
+     * @return true si la copie est un succès
+     */
     private static boolean copyFile(String from, String to) {
         try {
             File f = new File(to);
@@ -82,10 +111,23 @@ public class FileManager {
         return true;
     }
 
+    /**
+     * Permet de récupérer le chemin relatif d'un chemin absolu
+     * @param absolutePath Le chemin absolu
+     * @param initPath La racine
+     * @return String le chemin absolu
+     */
     private static String getRelativePath(String absolutePath, String initPath){
         return absolutePath.substring(initPath.length(), absolutePath.length());
     }
 
+    /**
+     * Permet de build de manière récursive (en allant dans chaque sous-dossier)
+     * @param directory File le répertoire à traiter
+     * @param initPath Le chemin initiale (le dossier racine du site qui contiendra le dossier build)
+     * @param initPathBuild Le chemin initiale du dossier build (souvent initPath/build)
+     * @return true si le build est un succès
+     */
     private static boolean buildRecursive(File directory, String initPath, String initPathBuild){
         for (final File fileEntry : directory.listFiles()) {
             if (fileEntry.isDirectory()) {
@@ -117,6 +159,11 @@ public class FileManager {
         return true;
     }
 
+    /**
+     * Permet de build un site
+     * @param path le chemin au dossier à build
+     * @return true si le build est un succès
+     */
     public static boolean build(String path){
         // Vérifie que les fichiers index et config existes
         if(!fileExists(path + File.separator + "index.md")){
